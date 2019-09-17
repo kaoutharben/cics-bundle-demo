ad274acfcf3cc8ce1a967de94bb8a48abb21c4a8
Stewart Francis <stewartfrancis@uk.ibm.com>
2019-09-17 12:31:44 +0100

    TSQify

--- demo-war/src/main/java/demo/SimpleServlet.java
+++ demo-war/src/main/java/demo/SimpleServlet.java
@@ -1,6 +1,7 @@
 package demo;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.ibm.cics.server.InvalidRequestException;
-import com.ibm.cics.server.Task;
+import com.ibm.cics.server.CicsConditionException;
+import com.ibm.cics.server.ItemHolder;
+import com.ibm.cics.server.TSQ;
 
 /**
  * Servlet implementation class SimpleServlet
@@ -27,15 +29,19 @@ public class SimpleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		
-		response.getWriter().print("Hello world!");
-		Task task = Task.getTask();
-
+		TSQ tsq = new TSQ();
+		tsq.setName(TSQ_NAME);
+		
+		ItemHolder itemHolder = new ItemHolder();
+		
 		try {
-		    String userid = task.getUSERID();
-		    response.setContentType("text/html");
-		    response.getWriter().print("\nI am " + userid);
-		} catch (InvalidRequestException e) {
-		    throw new RuntimeException(e);
+			tsq.readNextItem(itemHolder);
+			String value = new String(itemHolder.getValue(), StandardCharsets.UTF_8);
+			response.setContentType("text/html");
+			response.getWriter().print(value);
+			
+		} catch (CicsConditionException e) {
+			throw new RuntimeException(e);
 		}
     }
 
